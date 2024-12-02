fba6ec06b53ddecf6f173699c34f3cb70004d79e
rakazak <rakazakben@gmail.com>
2024-12-02 16:34:00 +0100

    test queryselector

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 //lancerJeu();
 let inputEcriture = document.getElementById("inputEcriture");
-console.log("inputEcriture");
+console.log(inputEcriture);
+let btnValiderMot = document.getElementById("btnValiderMot");
+console.log(btnValiderMot);
+
+let zoneProposition = document.querySelector(".zoneProposition");
+console.log(zoneProposition);
+let zoneScore = document.querySelector(".zoneScore span");
+console.log(zoneScore);
+
+let selecteurRadio = document.querySelectorAll(".optionSource input");
+console.log(selecteurRadio);
