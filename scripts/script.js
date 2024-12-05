24aa60301f03016c4728497ac876c27ed60d3a10
rakazak <rakazakben@gmail.com>
2024-12-05 11:39:38 +0100

    ajout choix proposition

--- scripts/script.js
+++ scripts/script.js
@@ -2,42 +2,56 @@ function afficherResultat(score, nbMots){
     let scoreboard = document.querySelector(".zoneScore span");
     scoreboard.textContent = `${score}/${nbMots}`;
 }
-function selecteur(){
-    let choix = prompt("choisissez phrases ou mots");
-    while(choix !== "mots" && choix !== "phrases"){
-        choix = prompt("voulez vous jouer avec des mots ou des phrases ?");
-    }
-    return choix;
+function afficherPropositions(mot){
+    let zoneProposition = document.querySelector(".zoneProposition");
+    zoneProposition.innerText = mot;
 }
-function lancerBoucleJeu(listePropositions){
-   let score = 0;
-   let motecrit
-    for(let i = 0; i<listePropositions.length; i++){
-        motecrit = prompt("entrez le mot : "+listePropositions[i]);
-        if(motecrit === listePropositions[i]){
-            score ++;
-            console.log("bonne reponse");
-    
+
+function lancerJeu(){
+    //let choix = selecteur();
+    let score =0;
+    let bouton = document.getElementById("btnValiderMot");
+    let reponse = document.getElementById("inputEcriture");
+    let listeProposition= listeMot;
+    let i = 0;
+
+
+    afficherPropositions(listeProposition[i]);
+    bouton.addEventListener("click", () => { 
+        if(reponse.value === listeProposition[i]){
+            score++;
+        }
+        i++;
+        
+        if(listeProposition[i] === undefined){
+            afficherPropositions("Le jeu est fini");
+            bouton.disabled = true;
         }
         else{
-            console.log("mauvaise reponse");
+            afficherPropositions(listeProposition[i]);
         }
+        console.log(reponse.value);
+       
+        reponse.value = "";
+        afficherResultat(score, i);
+
+        console.log("j'ai cliqué");
+        console.log(i);
+    });
+    
+    let radios = document.querySelectorAll(".optionSource input");
+
+    for(let k = 0; k< radios.length; k++){
+        radios[k].addEventListener("change", (event) =>{
+            if(event.target.value === "1"){
+                listeProposition = listeMot;
+            }
+            else{
+                listeProposition = listePhrase;
+            }
+            afficherPropositions(listeProposition[i]);
+        });
     }
-    return score;
-}
-function lancerJeu(){
-    let choix = selecteur();
-    let score =0;
-    let nbMots =0;
-    if(choix === "mots"){
-        nbMots = listeMot.length
-        score = lancerBoucleJeu(listeMot);
-    }
-    else{
-        score= lancerBoucleJeu(listePhrase);
-        nbMots = listePhrase.length
-    }
-    afficherResultat(score, nbMots);
 
 
 }
